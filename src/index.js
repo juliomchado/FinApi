15dff124d8e5d2971db554c032c0cfbf98478ea6
juliomchado <juliocarlos00@hotmail.com>
2021-12-14 20:51:29 +0000

    add/ create dummy user

--- src/index.js
+++ src/index.js
@@ -1,10 +1,41 @@
+const { response } = require('express');
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(express.json());
 
+const customers = [];
+
+/** 
+ * cpf - string
+ * name - string
+ * id - uuid
+ * statement []
+ */
+
+app.post("/account", (req, res) => {
+    const { cpf, name } = req.body;
+
+    const customerAlreadyExists = customers.some((customer) => customer.cpf === cpf);
+
+    if (customerAlreadyExists) {
+        return res.status(400).json({ error: "Customer already exists." });
+    }
+
+    customers.push({
+        id: uuidv4(),
+        cpf,
+        name,
+        statement: []
+    });
+
+    return res.status(201).send();
+
+});
+
 
 app.listen(3333, () => {
-    console.log('Server running !')
-})
+    console.log('Server running !');
+});
