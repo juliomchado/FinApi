bcf15de47523acb8ceec25556327818c9262a08f
juliomchado <juliocarlos00@hotmail.com>
2021-12-14 23:30:48 +0000

    add/ update account

--- src/index.js
+++ src/index.js
@@ -126,6 +126,24 @@ app.get("/statement/date", verifyIfExistsAccountCPF, (req, res) => {
 });
 
 
+app.put("/account", verifyIfExistsAccountCPF, (req, res) => {
+
+    const { name } = req.body;
+    const { customer } = req;
+
+    customer.name = name;
+
+    return res.status(204).send();
+
+});
+
+app.get("/account", verifyIfExistsAccountCPF, (req, res) => {
+
+    const { customer } = req;
+
+    return res.status(200).json(customer);
+})
+
 
 app.listen(3333, () => {
     console.log('Server running !');
