a52921f2e593ab8824a9b3ea002dee3291fc6d14
juliomchado <juliocarlos00@hotmail.com>
2021-12-14 21:22:32 +0000

    add/ get statement

--- src/index.js
+++ src/index.js
@@ -32,8 +32,21 @@ app.post("/account", (req, res) => {
     });
 
     return res.status(201).send();
-
 });
+
+app.get("/statement", (req, res) => {
+    const { cpf } = req.headers;
+
+    const customer = customers
+        .find((customer) => customer.cpf === cpf);
+
+    if (!customer) {
+        return res.status(400).json({ error: "Customer does not exists" });
+    }
+
+    return res.status(200).json(customer.statement);
+
+})
 
 
 app.listen(3333, () => {
